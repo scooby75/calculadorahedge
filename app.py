15043b08dfe104d152c3d5ab43d5d0421a398fda
scooby75 <45230721+scooby75@users.noreply.github.com>
2024-10-07 22:16:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 import streamlit as st
 
-def calcular_aposta_back(odd_lay, responsabilidade):
-    # Calcula o valor da aposta Lay
-    valor_aposta_lay = responsabilidade / (odd_lay - 1)
-    return valor_aposta_lay
-
-def mostrar_resultados(odd_lay, responsabilidade, odd_back):
-    valor_aposta_lay = calcular_aposta_back(odd_lay, responsabilidade)
-    
-    # Calcula o valor da aposta Back para cobrir a responsabilidade
-    valor_aposta_back = responsabilidade / (odd_back - 1)
-
-    st.write(f"Para uma responsabilidade de R${responsabilidade:.2f} e odd Lay de {odd_lay}:")
-    st.write(f"Você deve apostar R${valor_aposta_lay:.4f} no Lay.")
-    st.write(f"Para odds Back de {odd_back}, aposte R${valor_aposta_back:.4f} no Back para cobrir a responsabilidade.")
+def calcular_responsabilidade(odd_lay, valor_apostado):
+    # Calcula a responsabilidade da aposta Lay
+    responsabilidade = valor_apostado * (odd_lay - 1)
+    return responsabilidade
 
 # Título da aplicação
-st.title("Calculadora de Apostas")
+st.title("Calculadora de Responsabilidade de Apostas")
 
-# Inputs para odd Lay, responsabilidade e odd Back
+# Inputs para odd Back, valor apostado e odd Lay
+odd_back = st.number_input("Informe a odd Back:", min_value=1.0, format="%.2f")
+valor_apostado = st.number_input("Informe o valor apostado:", min_value=0.01, format="%.2f")
 odd_lay = st.number_input("Informe a odd Lay:", min_value=1.0, format="%.2f")
-responsabilidade = st.number_input("Informe a responsabilidade desejada:", min_value=0.01, format="%.2f")
-odd_back = st.number_input("Informe a odd Back após movimentação:", min_value=1.0, format="%.2f")
 
 # Botão para calcular
 if st.button("Calcular"):
-    mostrar_resultados(odd_lay, responsabilidade, odd_back)
+    responsabilidade = calcular_responsabilidade(odd_lay, valor_apostado)
+    st.write(f"A responsabilidade da aposta Lay é R${responsabilidade:.2f}.")
